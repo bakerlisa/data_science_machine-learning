73a1108aa87198a56fc4847a4b7f2f6ac7cb80ae
Lisa <lb.broadhead@gmail.com>
2022-05-02 15:02:30 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# 1. TASK: print "Hello World"
+x = "hello world"
+print(x)
+
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print("Hello " +  name)
+
+# 3. print "Hello 42!" with the number in a variable
+num = 42
+print ("Hello, ", num)	# with a comma
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {}".format(fave_food1)) # with .format()
+print(f"I love to eat {fave_food2}") # with an f string
