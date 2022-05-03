0112de04fce9b5bfd3a078114a9747a6513d5059
Lisa <lb.broadhead@gmail.com>
2022-05-03 15:32:24 -0500

    first commit

--- pre-botocamp/banking/BankAccount.py
+++ pre-botocamp/banking/BankAccount.py
@@ -1,6 +1,5 @@
 class BankAccount:
-    def __init__(self,name,int_rate=0.01,balance=0):
-        self.name = name
+    def __init__(self,int_rate=0.01,balance=0):
         self.int_rate = int_rate
         self.balance = balance
     
--- pre-botocamp/banking/User.py
+++ pre-botocamp/banking/User.py
@@ -1,7 +1,7 @@
 class User:
-    def __init__(self,name,account=0):
+    def __init__(self,name,amount=0):
         self.name = name
-        self.account = BankAccount(int_rate=0.02, balance=0)
+        self.account = BankAccount(int_rate=0.02, balance=amount)
     
     def make_withdrawal(self,amount):
         if(self.account - amount >= 0):
@@ -23,11 +23,6 @@ class User:
             print("Insufficicent funds")
 
 lisa = User("lisa",500)
-lisa.make_withdrawal(200)
-lisa.make_deposit(100)
-lisa.display_user_balance()
+
 
 natalie = User("natalie",200)
-natalie.make_withdrawal(100)
-natalie.make_deposit(200)
-natalie.display_user_balance()
