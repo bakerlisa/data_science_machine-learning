44ec945e3d0a916089ade44e9e15a9566774629b
Lisa <lb.broadhead@gmail.com>
2022-05-19 11:15:22 -0500

    first commit

---
+++ week1/notes/tuples.py
@@ -0,0 +1,13 @@
+#  Tuple Practice:
+
+z = (3,7,4,2)
+print(z[-1])
+print(z[1:2])
+
+friends = ('Steve', 'Rachel', 'Michael', 'Monica')
+for index, friend in enumerate(friends):
+    print(index,friend)
+
+animals = ("lama", "sheep","lama",28,"lama","monkey","donkey")
+
+print(f"lama count: {animals.count('lama')}")
