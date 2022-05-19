30b16f793c55a47997ecb2ee43a91f6fd7428e14
Lisa <lb.broadhead@gmail.com>
2022-05-19 12:07:27 -0500

    first commit

--- week1/notes/loops.py
+++ week1/notes/loops.py
@@ -1,15 +1,18 @@
-myName = "Lisa"
-# for x in myName:
-    # print(x)
+# myName = "Lisa"
+# # for x in myName:
+#     # print(x)
 
-dataScienceTools = ['Python', 'R', 'SQL', 'Git']
-print(len(dataScienceTools))
-for x in range(0,len(dataScienceTools)):
-    print(x)
+# dataScienceTools = ['Python', 'R', 'SQL', 'Git']
+# print(len(dataScienceTools))
+# for x in range(0,len(dataScienceTools)):
+#     print(x)
 
-for index,tool in enumerate(dataScienceTools):
-    print(index)
+# for index,tool in enumerate(dataScienceTools):
+#     print(index)
 
-string = "this is a string"
+# string = "this is a string"
 
-print(string.upper())
+# print(string.upper())
+
+x = "Hello"[0]
+print(x[0])
